da2081f757aa7d4055c2f0b097a8f1482ea5afb9
Mohamed Hossny <mhossny2@outlook.com>
2024-02-03 01:57:45 +0200

    feat: add preact greeting banner

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from "astro/config";
+import preact from "@astrojs/preact";
 
+// https://astro.build/config
 export default defineConfig({
   site: "https://astro-blog-510a.onrender.com",
+  integrations: [preact()]
 });
